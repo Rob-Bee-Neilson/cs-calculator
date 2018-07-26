11e8bc3199bf84f30930d31aeabda185e9ec4937
Richard-Boeve <richardboeve@hotmail.com>
2018-07-26 14:28:54 +1200

    added functionality

--- calculator.js
+++ calculator.js
@@ -4,22 +4,91 @@ var temp = ''
 
 //event listener for when a button on the calculator is clicked
   
-function input() {
-    var digitValue = document.getElementById("buttonId").getAttribute("value");
-    if (!isNaN(digitValue)) {
+function input(digitValue) {
+    if (!isNaN(digitValue)) {     
         temp += digitValue;
         console.log(temp);
-        //document.getElementById("answer").innerHTML = "1-3";
         document.getElementById("answer").value = temp;
     }
-     else if (digitValue === "+") {
+    else if (digitValue === ".") {   
+        temp += digitValue;
+        console.log(temp);
+        document.getElementById("answer").value = temp;
+    }    
+    else if (digitValue === "+") {
         select.push(temp);
         select.push("+");
-        console.log(temp);
+        console.log(select);
         temp = '';
         document.getElementById("answer").value = "+";
     }
-    
+    else if (digitValue === "-") {
+        select.push(temp);
+        select.push("-");
+        console.log(select);
+        temp = '';
+        document.getElementById("answer").value = "-";
+    }
+    else if (digitValue === "/") {
+        select.push(temp);
+        select.push("/");
+        console.log(select);
+        temp = '';
+        document.getElementById("answer").value = "/";
+    }
+    else if (digitValue === "x") {
+        select.push(temp);
+        select.push("*");
+        console.log(select);
+        temp = '';
+        document.getElementById("answer").value = "x";
+    }
+    else if (digitValue === "%") {
+        //select.push(temp);
+        //select.push("");
+        console.log(select);
+        //temp = '';
+        document.getElementById("answer").value = "No Clue";
+    }
+    else if (digitValue === "AC") {
+        temp = '';
+        select = [];
+        total = 0
+        document.getElementById("answer").value = total;
+    }   
+    else if (digitValue === "CE") {
+        temp = '';
+        document.getElementById("answer").value = total;
+    } 
+    else if (digitValue === "=") {
+        select.push(temp);
+        var calc = Number(select[0]);
+        for (var i = 0; i < select.length; i++) {
+            var nextNum = Number(select[i+1]);
+            var operator = select[i];
+
+            if (operator === '+') {
+                calc += nextNum;
+            console.log(select);
+            }
+
+        }    
+
+
+
+
+
+
+        document.getElementById("answer").value = calc;
+    }  
+
+
+
+
+
+
+
+
 }
 
 
